833d3b2ab055a3ba110b50a27e718bfa01b709f8
Hudson Prestidge <charcon@ihug.co.nz>
2016-10-11 16:22:57 +1300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatesAndTimes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
